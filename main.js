86b35ad260866526278bbe4fbde52a118de6ad34
Lucas Carrari <lucascarrari@gmail.com>
2024-06-04 14:32:58 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,397 @@
+let carta1 = {
+    nome: "Chaves",
+    imagem:
+      "https://lcmagalhaes.com.br/wp-content/uploads/2020/09/chaves.png",
+    atributos: {
+      Força: 7,
+      Agilidade: 5,
+      Inteligência: 3,
+      Resistência: 9,
+      Coragem: 4,
+      Magia: 8
+    }
+  };
+  let carta2 = {
+    nome: "Quico",
+    imagem:
+      "https://uploads.bemparana.com.br/upload/image/noticia/noticia_962442_img1_quico_seriado.jpg",
+    atributos: {
+      Força: 6,
+      Agilidade: 5,
+      Inteligência: 2,
+      Resistência: 8,
+      Coragem: 5,
+      Magia: 9
+    }
+  };
+  let carta3 = {
+    nome: "Chiquinha",
+    imagem:
+      "https://www.areavip.com.br/wp-content/uploads/2019/09/chiquinha.jpg",
+    atributos: {
+      Força: 5,
+      Agilidade: 7,
+      Inteligência: 9,
+      Resistência: 4,
+      Coragem: 7,
+      Magia: 7
+    }
+  };
+  let carta4 = {
+    nome: "Seu Madruga",
+    imagem:
+      "https://imagens3.ne10.uol.com.br/blogsne10/social1/uploads/2021/05/Ramon-Valdes-como-Seu-Madruga-em-Chaves.jpg",
+    atributos: {
+      Força: 9,
+      Agilidade: 8,
+      Inteligência: 4,
+      Resistência: 9,
+      Coragem: 5,
+      Magia: 5
+    }
+  };
+  let carta5 = {
+    nome: "Dona Florinda",
+    imagem:
+      "https://i.pinimg.com/736x/f5/be/35/f5be35d7b42d86bbe4125b07073c9a18.jpg",
+    atributos: {
+      Força: 10,
+      Agilidade: 5,
+      Inteligência: 4,
+      Resistência: 3,
+      Coragem: 5,
+      Magia: 3
+    }
+  };
+  let carta6 = {
+    nome: "Prof. Girafales",
+    imagem:
+      "https://imagens.brasil.elpais.com/resizer/Duhm0FSD24yWySjNvbv_-_aw4G4=/414x0/arc-anglerfish-eu-central-1-prod-prisa.s3.amazonaws.com/public/WTH33T43JQ4HCKAS6BHGGBYREQ.jpg",
+    atributos: {
+      Força: 6,
+      Agilidade: 7,
+      Inteligência: 10,
+      Resistência: 5,
+      Coragem: 5,
+      Magia: 4
+    }
+  };
+  let carta7 = {
+    nome: "Bruxa do 71",
+    imagem:
+      "https://aventurasnahistoria.uol.com.br/media/_versions/bruxa71annd_widelg.jpg",
+    atributos: {
+      Força: 4,
+      Agilidade: 3,
+      Inteligência: 3,
+      Resistência: 2,
+      Coragem: 4,
+      Magia: 10
+    }
+  };
+  let carta8 = {
+    nome: "Nhonho",
+    imagem:
+      "https://aventurasnahistoria.uol.com.br/media/_versions/personagem/nhonho_capa_widelg.jpg",
+    atributos: {
+      Força: 9,
+      Agilidade: 1,
+      Inteligência: 6,
+      Resistência: 3,
+      Coragem: 1,
+      Magia: 5
+    }
+  };
+  let carta9 = {
+    nome: "Seu Barriga",
+    imagem:
+      "http://1.bp.blogspot.com/-VyvNC0I5Mco/ViPfWRkz_nI/AAAAAAAAAmw/bujANDzX5Ow/s1600/seu-barriga.jpg",
+    atributos: {
+      Força: 10,
+      Agilidade: 2,
+      Inteligência: 8,
+      Resistência: 9,
+      Coragem: 2,
+      Magia: 2
+    }
+  };
+  let carta10 = {
+    nome: "Godines",
+    imagem:
+      "https://img.estadao.com.br/resources/jpg/3/8/1529523483583.jpg",
+    atributos: {
+      Força: 2,
+      Agilidade: 1,
+      Inteligência: 2,
+      Resistência: 3,
+      Coragem: 4,
+      Magia: 4
+    }
+  };
+  let carta11 = {
+    nome: "Popis",
+    imagem:
+      "https://www.teleseries.com.br/wp-content/uploads/2011/07/popis.jpg",
+    atributos: {
+      Força: 3,
+      Agilidade: 4,
+      Inteligência: 5,
+      Resistência: 6,
+      Coragem: 4,
+      Magia: 4
+    }
+  };
+  let carta12 = {
+    nome: "Jaiminho",
+    imagem:
+      "https://t.ctcdn.com.br/UhRrF0Iyp1lNrqEjyl2YBOo4CwQ=/665x374/smart/filters:format(webp)/i215798.png",
+    atributos: {
+      Força: 0,
+      Agilidade: 0,
+      Inteligência: 0,
+      Resistência: 0,
+      Coragem: 0,
+      Magia: 1,
+      
+    }
+  };
+  let carta13 = {
+    nome: "Sr. Furtado",
+    imagem:
+      "https://imagem.natelinha.uol.com.br/original/senhorfurtado-chaves_c7a3fd0f2c3428181d7c57bbcef0323133d8cc25.jpeg",
+    atributos: {
+      Força: 6,
+      Agilidade: 10,
+      Inteligência: 8,
+      Resistência: 7,
+      Coragem: 4,
+      Magia: 4
+    }
+  };
+  let carta14 = {
+    nome: "Dona Neves",
+    imagem:
+      "https://img.estadao.com.br/resources/jpg/9/4/1529519093749.jpg",
+    atributos: {
+      Força: 2,
+      Agilidade: 3,
+      Inteligência: 9,
+      Resistência: 1,
+      Coragem: 10,
+      Magia: 2
+    }
+  };
+  let carta15 = {
+    nome: "Pati",
+    imagem:
+      "https://uploads.metropoles.com/wp-content/uploads/2018/07/18171916/paty.jpg",
+    atributos: {
+      Força: 1,
+      Agilidade: 1,
+      Inteligência: 2,
+      Resistência: 3,
+      Coragem: 4,
+      Magia: 2
+    }
+  };
+  let carta16 = {
+    nome: "Chapolin",
+    imagem:
+      "https://kanto.legiaodosherois.com.br/w760-h398-cfill/wp-content/uploads/2016/03/ef4c82258bc2660bbb21774c0cdd97f5.jpg.jpeg",
+    atributos: {
+      Força: 10,
+      Agilidade: 10,
+      Inteligência: 10,
+      Resistência: 10,
+      Coragem: 1,
+      Magia: 10
+    }
+  };
+  
+  let baralho = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14, carta15, carta16]
+  let meuBaralho = []
+  
+  function distribuiCartas(){
+      for (let minhasCartas = 0; minhasCartas < 8; minhasCartas++){
+          let indiceAleatorio = Math.floor(Math.random() * baralho.length)
+  
+          if(indiceAleatorio >= baralho.length && indiceAleatorio >= baralho.length){
+          indiceAleatorio = (baralho.length-1)
+          }
+  
+          meuBaralho.push(baralho.splice(indiceAleatorio, 1)[0]);
+          
+      }
+  }
+  
+  distribuiCartas()
+  
+  let cartaMaquina
+  let cartaJogador
+  let minhaCarta = document.querySelector('#cartaEscolhida')
+  let campoJogador = document.querySelector('.cartaJogador')
+  let campoMaquina = document.querySelector('.cartaMaquina')
+  let pontos = document.querySelector('.pontuacao')
+  let versus = document.querySelector('.versus')
+  
+  pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`
+  
+  
+  function sortearCarta(){    
+          numeroCartaMaquina = Math.floor(Math.random() * baralho.length) 
+          numeroCartaJogador = Math.floor(Math.random() * meuBaralho.length)
+  
+          if(numeroCartaMaquina >= baralho.length && numeroCartaJogador >= meuBaralho.length){
+              numeroCartaMaquina = (baralho.length-1)
+          }
+          cartaMaquina = baralho[numeroCartaMaquina]
+  
+          cartaJogador = meuBaralho[numeroCartaJogador]
+          document.getElementById('campoBatalha').style.display="none"
+          document.getElementById('btnSortear').style.display="none"
+          document.getElementById('btnJogar').disabled = false
+          document.getElementById('escolhaAtributo').style.display="block"
+  
+          minhaCarta.style.display = 'block'
+  
+          minhaCarta.innerHTML = `
+          <div class='carta'>
+          <div class="nomeCarta">${cartaJogador.nome}</div>
+          <img class="imgCarta" src="${cartaJogador.imagem}" alt="">             
+          <div class="atributosCarta">
+          <div class="bg1"><span><input  type='radio' > Força</span><span>${cartaJogador.atributos.Força}</span></div>
+          <div class="bg2"><span><input  type='radio'> Agilidade</span><span>${cartaJogador.atributos.Agilidade}</span></div>
+          <div class="bg1"><span><input  type='radio'> Inteligência</span><span>${cartaJogador.atributos.Inteligência}</span></div>
+          <div class="bg2"><span><input  type='radio'> Resistência</span><span>${cartaJogador.atributos.Resistência}</span></div>
+          <div class="bg1"><span><input  type='radio'> Coragem</span><span>${cartaJogador.atributos.Coragem}</span></div>
+          <div class="bg2"><span><input  type='radio'> Magia</span><span>${cartaJogador.atributos.Magia}</span></div>
+          
+          </div></div>` 
+          exibirOpcoes()
+                             
+  }
+  
+  
+  
+  function exibirOpcoes(){
+      let opcoes = document.getElementById('opcoes')
+      let opcoesTexto = ''
+      for (let atributo in cartaJogador.atributos){
+          opcoesTexto += `<div><input  type='radio' name='atributo' value='${atributo}'>${atributo}</div>`
+      }
+      opcoes.innerHTML = opcoesTexto
+  }
+  
+  function obtemAtributoSelecionado(){
+      let radioAtributo = document.getElementsByName('atributo')
+      for (let i = 0; i < radioAtributo.length; i++){
+          if(radioAtributo[i].checked){
+              return radioAtributo[i].value
+          }
+      }
+      
+  }
+  
+  let resultado = document.querySelector('#resultado')
+  
+  function jogar(){
+      let atributoSelecionado =  obtemAtributoSelecionado()
+      minhaCarta.innerHTML = ''
+      campoJogador.innerHTML = `
+      <div class='carta'>
+      <div class="nomeCarta">${cartaJogador.nome}</div>
+      <img class="imgCarta" src="${cartaJogador.imagem}" alt="">             
+      <div class="atributosCarta">
+       <div class="bg1"><span>Força</span><span>${cartaJogador.atributos.Força}</span></div>
+          <div class="bg2"><span>Agilidade</span><span>${cartaJogador.atributos.Agilidade}</span></div>
+          <div class="bg1"><span>Inteligência</span><span>${cartaJogador.atributos.Inteligência}</span></div>
+          <div class="bg2"><span>Resistência</span><span>${cartaJogador.atributos.Resistência}</span></div>
+          <div class="bg1"><span>Coragem</span><span>${cartaJogador.atributos.Coragem}</span></div>
+          <div class="bg2"><span>Magia</span><span>${cartaJogador.atributos.Magia}</span></div>
+      </div></div>`
+  
+      versus.innerHTML = `<img class="img-x" src="https://i.imgur.com/zRf2J4T.png" alt="">`
+  
+      campoMaquina.innerHTML = `
+      <div class='carta'>
+      <div class="nomeCarta">${cartaMaquina.nome}</div>
+      <img class="imgCarta" src="${cartaMaquina.imagem}" alt="">             
+      <div class="atributosCarta">
+       <div class="bg1"><span>Força</span><span>${cartaMaquina.atributos.Força}</span></div>
+          <div class="bg2"><span>Agilidade</span><span>${cartaMaquina.atributos.Agilidade}</span></div>
+          <div class="bg1"><span>Inteligência</span><span>${cartaMaquina.atributos.Inteligência}</span></div>
+          <div class="bg2"><span>Resistência</span><span>${cartaMaquina.atributos.Resistência}</span></div>
+          <div class="bg1"><span>Coragem</span><span>${cartaMaquina.atributos.Coragem}</span></div>
+          <div class="bg2"><span>Magia</span><span>${cartaMaquina.atributos.Magia}</span></div>
+      </div></div>`
+      
+      if(cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]){
+          resultado.innerHTML = 'Você ganhou!'
+          meuBaralho.push
+          meuBaralho.push(baralho.splice(numeroCartaMaquina, 1)[0]);
+  
+      } else if(cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]){
+          resultado.innerHTML = 'Você perdeu!'
+          baralho.push(meuBaralho.splice(numeroCartaJogador, 1)[0]);
+      } else{
+          resultado.innerHTML = 'Empate'
+      }
+      
+      document.getElementById('campoBatalha').style.display="flex"
+      document.getElementById('escolhaAtributo').style.display="none"
+      document.getElementById('btnJogar').disabled = true
+      document.getElementById('btnNovoJogo').disabled = false
+      pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`
+  }
+  
+  
+  function novoJogo(){
+  
+      if(baralho.length == 0){
+          resultado.innerHTML = `As cartas do seu oponente acabaram, e você ganhou o jogo.<br> Parabéns!`
+          baralho = meuBaralho
+          escodeButton()
+      }else if(meuBaralho.length == 0){
+          resultado.innerHTML = `As cartas acabaram, e você perdeu o jogo.<br> Boa sorte na próxima vez`
+          escodeButton()
+      }else{
+          document.getElementById('btnNovoJogo').disabled = true
+          minhaCarta.style.display = "none"
+          document.getElementById('escolhaAtributo').style.display="none" 
+          campoJogador.innerHTML = ''
+          versus.innerHTML = ''
+          campoMaquina.innerHTML = ''
+          resultado.innerHTML = ''
+          sortearCarta()
+          pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`
+      }
+  }
+  
+  function reiniciarJogo(){
+      meuBaralho = []
+      distribuiCartas()
+      pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`
+      resultado.innerHTML = ''
+      document.getElementById('btnNovoJogo').disabled = true 
+      mostraButton()
+  
+  }
+  
+  function escodeButton(){
+      document.getElementById('reiniciarJogo').style.display="inline-block"
+      document.getElementById('btnJogar').style.display="none"
+      document.getElementById('btnNovoJogo').style.display="none" 
+      campoJogador.innerHTML = ''
+      versus.innerHTML = ''
+      campoMaquina.innerHTML = ''
+  
+  }
+  
+  function mostraButton(){
+      document.getElementById('reiniciarJogo').style.display="none"
+      document.getElementById('btnJogar').style.display="inline-block"
+      document.getElementById('btnNovoJogo').style.display="inline-block" 
+      document.getElementById('btnSortear').disabled = false
+      document.getElementById('btnSortear').style.display="inline-block"
+      document.getElementById('reiniciarJogo').style.display="none" 
+  }
